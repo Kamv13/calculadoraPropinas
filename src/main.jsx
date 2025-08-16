3fde7cf02be14d1596f60ea92f4fe1f43f564ded
Kevin Mendoza <ksmv233@gmail.com>
2025-08-16 14:08:37 -0600

    finalizacioin instalacion sweetalert2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
